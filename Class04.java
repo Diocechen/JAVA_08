e427b75c48ae7a97eba41f1009c9c0afe8057bfc
Diocechen <78402906+Diocechen@users.noreply.github.com>
2022-09-09 13:46:17 +0800

    Update Class04.java

--- Class04.java
+++ Class04.java
@@ -3,7 +3,7 @@ package Java08;
 class CBox{
 	int length;
 	int width;
-	int heigth;
+	int height;
 }
 
 public class Class04 {
@@ -11,13 +11,13 @@ public class Class04 {
 	public static void main(String[] args) {
 		CBox box;
 		box = new CBox();
-		box.heigth = 25;
+		box.height = 25;
 		box.width = 10;
 		box.length = 15;
 		
 		System.out.println("length = "+box.length);
 		System.out.println("width = "+box.width);
-		System.out.println("heigth = "+box.heigth);
+		System.out.println("heigth = "+box.height);
 		
 	}
 
